de68f1d4c83a7a7b1f9274aa7c890f8264348472
James Edward Gray II <james@graysoftinc.com>
2025-04-18 11:11:09 -0500

    Tesing remote cluster.

--- mix.exs
+++ mix.exs
@@ -61,7 +61,7 @@ defmodule Kanban.MixProject do
       {:telemetry_poller, "~> 1.0"},
       {:gettext, "~> 0.26"},
       {:jason, "~> 1.2"},
-      {:dns_cluster, "~> 0.1.1"},
+      {:dns_cluster, "~> 0.2.0"},
       {:bandit, "~> 1.5"},
       {:dialyxir, "~> 1.4", only: [:dev, :test], runtime: false}
     ]
